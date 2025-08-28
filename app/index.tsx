8284ccfbae21e0375fea2a91d48d87d68307e5b5
kijiwon <kiji1313@gmail.com>
2025-08-28 19:10:42 +0900

    header button 조건부 스타일링

--- app/index.tsx
+++ app/index.tsx
@@ -1,40 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  Pressable,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-  View,
-} from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { theme } from "../assets/colors";
 
 export default function HomeScreen() {
+  const [working, setWorking] = useState(true);
+
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
       <View style={styles.header}>
-        <TouchableOpacity activeOpacity={0}>
-          <Text style={styles.btnText}>Work</Text>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
+          >
+            Work
+          </Text>
         </TouchableOpacity>
-        <TouchableHighlight
-          onPress={() => console.log("pressed")}
-          underlayColor={"red"}
-          activeOpacity={0.5}
-        >
-          <Text style={styles.btnText}>Travel</Text>
-        </TouchableHighlight>
-      </View>
-      <View style={styles.header}>
-        {/* UI 변화 없음(시각적 피드백x) */}
-        <TouchableWithoutFeedback onPress={() => console.log("no feedback!")}>
-          <Text style={styles.btnText}>no피드백</Text>
-        </TouchableWithoutFeedback>
-        {/* press 상호작용 감지 - 컴포넌트 주변의 press를 감지해 동작할 수 있음*/}
-        <Pressable>
-          <Text style={styles.btnText}>Press</Text>
-        </Pressable>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{ ...styles.btnText, color: working ? theme.grey : "white" }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
