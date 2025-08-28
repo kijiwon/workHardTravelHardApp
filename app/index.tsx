ab9d4833067813240354b503322f17beaf46b25e
kijiwon <kiji1313@gmail.com>
2025-08-28 17:39:54 +0900

    header basic style

---
+++ app/index.tsx
@@ -0,0 +1,33 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { theme } from "../assets/colors";
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <View style={styles.header}>
+        <Text style={styles.btnText}>Work</Text>
+        <Text style={styles.btnText}>Travel</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 100,
+  },
+  btnText: {
+    color: "white",
+    fontSize: 38,
+    fontWeight: 600,
+  },
+});
